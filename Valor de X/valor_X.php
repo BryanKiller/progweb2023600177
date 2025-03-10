9aab739a122421dcb7ea37e58883181dd143b274
Bryankiller <entzanavegabryan18@gmail.com>
2025-03-10 10:18:45 -0600

    ValorX

---
+++ Valor de X/valor_X.php
@@ -0,0 +1,29 @@
+<?php
+// evitamos el 0
+$x = $_GET['x'] ? (float)$_GET['x'] : 1;
+
+// Calcular el numerador y denominador de la expresión
+$arriba = ($x ** 2 / 2) + (1 / sqrt($x));
+$abajo = 3 + (1 / 2) * ($x ** 3);
+
+//calculamos el resultado final
+$resultado = sqrt($arriba / $abajo);
+?>
+
+<html>
+<head>
+    <title>Calculo de X</title>
+</head>
+<body>
+
+    <h2 style="text-align: center;">Evaluación de la Expresión Matemática</h2>
+
+    <p  style="text-align: center;">Calcula:</p>
+    <img src="XX.jpg" alt="formula" style="display: block; margin: auto;>
+
+    <p style="text-align: center;">Para X =<?php echo $x; ?>, el resultado es: <?php echo $resultado; ?></p>
+
+
+</body>
+</html>
+
