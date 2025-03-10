e9eda8d6b258f74126e8124a13892f358003da35
Bryankiller <entzanavegabryan18@gmail.com>
2025-03-10 10:34:19 -0600

    CalcularX

--- Valor de X/valor_X.php
+++ Valor de X/valor_X.php
@@ -1,6 +1,6 @@
 <?php
 // evitamos el 0
-$x = $_GET['x'];
+$x = $_GET['x'] ? (float)$_GET['x'] : 1;
 
 // Calcular el numerador y denominador de la expresión
 $arriba = ($x ** 2 / 2) + (1 / sqrt($x));
